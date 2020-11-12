03ad081be6112d9726cd723558a64876d4052044
Sue Ceklosky <cekloskys@chc.edu>
2020-11-12 15:07:20 -0500

    Creating Multiple Actors At Once

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -7,12 +7,20 @@ class ActorsController < ApplicationController
 	end
 
 	def create
-		actor = Actor.new(actor_params)
-		if actor.save
+		flag = false
+		for a in 0..1 do
+			fields = Array[:actor1, :actor2]
+			actor = Actor.new(params.require(fields[a]).permit(:first_name, :last_name))
+			if actor.save
+				flag = true
+			else
+				flash[:errors] = actor.errors.full_messages
+				redirect_to "/actors/new"
+			end
+		end
+
+		if flag == true
 			redirect_to "/actors"
-		else
-			flash[:errors] = actor.errors.full_messages
-			redirect_to "/actors/new"
 		end
 	end
 
@@ -41,7 +49,11 @@ class ActorsController < ApplicationController
 	end
 
 	private
-	def actor_params
-		params.require(:actor).permit(:first_name, :last_name)
+	def actor_params1
+		params.require(:actor1).permit(:first_name, :last_name)
+	end
+
+	def actor_params2
+		params.require(:actor2).permit(:first_name, :last_name)
 	end
 end
